11819502236e8f60507b1029ba8f5095952f92b1
mathiashusted <username@users.noreply.github.com>
2023-10-29 17:44:54 +0100

    BMH algorithm finished

--- src/a1_mathias/aufgabe1.cpp
+++ src/a1_mathias/aufgabe1.cpp
@@ -1,6 +1,15 @@
 
 #include "aufgabe1.h"
 
+uint32_t Horspool::getShift_(const char last_char) const {
+  return (patternMap.find(last_char) != patternMap.end()) ? patternMap.at(last_char) : 0;
+  // Here we first check that the argument isn't out of range in our lookup table.
+  // If it is out of range, then we return a default value of 0.
+  // This is the behaviour we want because of the following 2 cases:
+  //    1. The character isn't in the lookup table at all -> skip the whole text (... and avoid throwing an exception :D)
+  //    2. The character's index+1 is the length of the table itself -> skip the whole text
+}
+
 
 // This function will generate a lookup table and store the pattern
 void Horspool::setPattern(const std::string& pat) {
@@ -25,10 +34,13 @@ std::vector<size_t> Horspool::getHits(const std::string& text) const {
   uint32_t j = 0;
   uint32_t m = pattern.length();
   uint32_t n = text.length();
+  uint32_t shift = 0;
   std::vector<size_t> output{};
+  // Here we initialize all variables to avoid compiler complications... (looking at you, macOS🤨)
 
   while (pos <= (n - m) && !(text.empty()) && !(pattern.empty())) {
     j = m;
+    shift = 0;
 
     while (j > 0 && (pos + j) <= n && text[pos+j-1] == pattern[j-1])
       j--;
@@ -36,11 +48,11 @@ std::vector<size_t> Horspool::getHits(const std::string& text) const {
     if (j == 0)
       output.push_back(pos);
 
+    shift = getShift_(text[pos+m-1]); // Here we call the internal getShift_ function
 
-    if (patternMap.find(text[pos+m-1]) != patternMap.end() && patternMap.at(text[pos+m-1]) != 0) 
-      pos = pos + patternMap.at(text[pos+m-1]);
-    else 
-      pos = pos + m;
+    alignCheck_(pos); // Internal function. Perform checks...
+
+    pos = (shift != 0) ? (pos + shift) : (pos + m);
   }
   return output;
 }
