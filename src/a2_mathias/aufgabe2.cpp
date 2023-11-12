9dd9fede3eab57ae77dd1b01400dbedc81ae6496
mathiashusted <mathias.husted@protonmail.com>
2023-11-12 18:50:38 +0100

    Further improved average runtime by around 16%

--- src/a2_mathias/aufgabe2.cpp
+++ src/a2_mathias/aufgabe2.cpp
@@ -8,18 +8,21 @@ void construct(std::vector<uint32_t>& sa, const std::string& text) {
 
     uint32_t len = text.length();
     sa.resize(len);
+    size_t counter = 0;
 
     for (uint32_t i = 0; i < len; i++)
         sa[i] = i;
 
-    // Sorting our array with a custom lambda expression (as per cppreference.com) which sorts by our second pair value (the string part)
-    // &text is captured, so that we can use it for lookup inside of our lambda function
-    // a and b are automatically taken from sa, which holds values from 1..len (our suffix "cursor" positions)
-    // Then we base our sorting (return value) on the alphabetical sorting from positions begin + a and begin + b and apply it to sa
-    std::sort(sa.begin(), sa.end(), [&text](const uint32_t & a, const uint32_t & b) {
-        return (std::lexicographical_compare((text.begin() + a), text.end(), (text.begin() + b), text.end()));
+    // Sorting our array with a custom lambda expression (as per cppreference.com) which sorts by comparing the text of two sa entries
+    // &text and &counter are captured, so that we can use them for lookup inside of our lambda function
+    // a and b are automatically taken from sa, which holds values from 1..len (our suffix start positions)
+    // Two entries are compared by their ASCII values and return 1 if the a-suffix is smaller than the b-suffix, otherwise 0.
+    std::sort(sa.begin(), sa.end(), [&text, &counter](const uint32_t & a, const uint32_t & b) {
+        for (counter = 0; text[a+counter] == text[b+counter];)
+            counter++;
+
+        return (text[a+counter] < text[b+counter]);
     });
-    //lexicographical_compare function found: https://www.appsloveworld.com/cplus/100/23/is-there-a-standard-way-to-compare-two-ranges-in-c
     return;
 }
 
--- src/a2_mathias/aufgabe2_experimental.cpp
+++ src/a2_mathias/aufgabe2_experimental.cpp
@@ -9,19 +9,22 @@ Thus, this change has been implemented in aufgabe2.cpp.
 However, it seems further improvements are still needed to achieve the targeted complexity of (n*logn).
 
 UPDATE: It seems that we can now achieve O(nlogn)
+
+UPDATE 2: It seems that a custom function is even faster than using lexicographical_map
 */
 
 #include "aufgabe2.hpp"
 
 #include <cstdint>
 #include <algorithm>
-
+#include <iostream>
 
 
 void construct(std::vector<uint32_t>& sa, const std::string& text) {
 
     uint32_t len = text.length();
     sa.resize(len);
+    size_t counter;
 
     for (uint32_t i = 0; i < len; i++)
         sa[i] = i;
@@ -30,10 +33,12 @@ void construct(std::vector<uint32_t>& sa, const std::string& text) {
     // &text is captured, so that we can use it for lookup inside of our lambda function
     // a and b are automatically taken from sa, which holds values from 1..len (our suffix "cursor" positions)
     // Then we base our sorting (return value) on the alphabetical sorting from positions begin + a and begin + b and apply it to sa
-    std::sort(sa.begin(), sa.end(), [&text](const uint32_t & a, const uint32_t & b) {
-        return (std::lexicographical_compare((text.begin() + a), text.end(), (text.begin() + b), text.end()));
+    std::sort(sa.begin(), sa.end(), [&text, &counter](const uint32_t & a, const uint32_t & b) {
+        for (counter = 0; text[a+counter] == text[b+counter];)
+            counter++;
+
+        return (text[a+counter] < text[b+counter]);
     });
-    //lexicographical_compare function found: https://www.appsloveworld.com/cplus/100/23/is-there-a-standard-way-to-compare-two-ranges-in-c
     return;
 }
 
